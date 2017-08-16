0b0a65268d2a242e4176ccefde11fcc83d57b603
vadim <va-demen@yandex.ru>
2017-08-16 09:07:12 +0400

    завершаю стили сайдбара

--- js/scripts.js
+++ js/scripts.js
@@ -10,3 +10,12 @@ $(function () {
 		$(".main-menu .navbar-form").slideToggle();
 	});
 });
+$(document).ready(function () {
+  var carouselCaptionWidth = $('#carousel-sidebar .carousel-inner .active img').width();
+	$('#carousel-sidebar .carousel-inner img').each(function () {
+    $('this').attr('width', carouselCaptionWidth);
+	});
+	$('#carousel-sidebar .sidebar-carousel-caption').css('max-width', carouselCaptionWidth + 'px');
+	$('#carousel-sidebar .carousel-indicators').css('max-width', carouselCaptionWidth + 'px');
+	$('.sidebar .banner').css('max-width', carouselCaptionWidth + 'px');
+});
